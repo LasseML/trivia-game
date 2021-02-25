96f46b21132b4762cd9eac0512bd1d354df5c3c1
Hunor Vadasz-Perhat <hunor.vadasz@gmail.com>
2021-02-25 15:14:07 +0100

    feat: add first features

---
+++ src/components/QuestionArea.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+<!--         <h1 v-for="question of questions"
+        :key="question.ID"
+        >{{question.question}}</h1> -->
+        <h1>{{questions[0].question}}</h1>
+        <h1 v-for="answer of questions[0].incorrect_answers"
+        :key="answer"
+        >{{answer}}</h1>
+        <h1>{{questions[0].correct_answer}}</h1>
+    </div>
+</template>
+
+<script>
+
+import { getQuestions } from "../utils/apiConnector";
+
+export default {
+  components: {
+    
+  },
+  data() {
+    return {
+        questions: []
+    };
+  },
+  created() {
+    this.loadDataFromAPI();
+  },
+  methods: {
+    async loadDataFromAPI() {
+      //load from api call
+      this.questions = await getQuestions();
+    },
+  },
+};
+
+</script>
---
+++ src/utils/apiConnector.js
@@ -0,0 +1,7 @@
+const QUESTION_URL = "https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple"
+
+export const getQuestions = () => {
+    return fetch(QUESTION_URL)
+    .then(response => response.json())
+    .then(response => response.results)
+}
